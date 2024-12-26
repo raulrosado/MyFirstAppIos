c55b6648cb007fec1bcd75b014c3866746b4f712
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2024-12-26 16:43:20 -0600

    new app

---
+++ MyFirstApp/Pages/MainPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainPage.swift
+//  MyFirstApp
+//
+//  Created by Raul Remedios Rosado on 12/26/24.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPage()
+}
