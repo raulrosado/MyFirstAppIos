8f253f3369653545258650c96214489442ed3c2b
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2024-12-27 22:55:02 -0600

    Add: app maps

---
+++ MyFirstApp/Maps/Maps.swift
@@ -0,0 +1,18 @@
+//
+//  Maps.swift
+//  MyFirstApp
+//
+//  Created by Raul Remedios Rosado on 12/27/24.
+//
+
+import SwiftUI
+
+struct Maps: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Maps()
+}
